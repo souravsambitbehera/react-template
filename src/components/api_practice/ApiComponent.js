2385c0e562096b598c01f5b79e30b7d2bfbe04ef
sourav sambit behera <souravsambit12@gmail.com>
2023-04-06 11:27:15 +0530

    api

--- src/components/api_practice/ApiComponent.js
+++ src/components/api_practice/ApiComponent.js
@@ -1,8 +1,36 @@
-const ApiComponent = ()=>{
-    return
-    (
-        <>
-            Api Component
-        </>
-    )
+import React, { useState } from 'react'
+
+const ApiComponent = () => {
+
+    const[value,setValue] = useState('');
+    const[username,setUsername] = useState('');
+    const[email,setEmail] = useState('');
+
+    const HandelUserClick = () =>{
+        alert("just click")
+    }
+    const HandelUsernameChange = (e)=>{
+        setUsername(e.target.value)
+    }
+    const HandelEmailChange = (e)=>{
+        setEmail(e.target.value)
+    }
+  return (
+    <div>
+        <button onClick={HandelUserClick}>click me</button>
+        <div>
+            <dl>   
+                <dt> Username</dt>
+                <dd><input type="text" onChange={HandelUsernameChange}  /></dd>
+                <dt>Email</dt>
+                <dd><input type="email" onChange={HandelEmailChange} /></dd>
+
+            </dl>
+            <p>username is {username} and his/her email id is {email}</p>
+        </div>
+    </div>
+  )
 }
+
+
+export default  ApiComponent
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import AppLayout from './AppLayout';
 import { RouterProvider } from 'react-router-dom';
 import {AppRouter} from './AppLayout'
+import ApiComponent from './components/api_practice/ApiComponent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={AppRouter} />
+    {/* <RouterProvider router={AppRouter} /> */}
+    <ApiComponent />
   </React.StrictMode>
 );
